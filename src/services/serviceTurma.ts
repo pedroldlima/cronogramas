026b8199f1bfa74b68d47ad43d45efb56980ab6d
Pedro Lucas <pedroldlima52@gmail.com>
2023-06-01 11:11:35 -0300

    Update

--- src/services/serviceTurma.ts
+++ src/services/serviceTurma.ts
@@ -1,7 +1,40 @@
 import Turma from "../databases/models/turma"
 import { AppDataSource } from "../databases/connections/data-source"
 
+
 const cursor = AppDataSource.getRepository(Turma)
+
+type newTurmaRequest = {
+    id_turma: string
+    fk_curso: string
+    data_inicio: Date
+    data_fim: Date
+    horas_aula_dia: Number
+}
+
+export class CreateTurmaService {
+    async execute({
+        id_turma,
+        fk_curso,
+        data_inicio,
+        data_fim,
+        horas_aula_dia,
+    }: newTurmaRequest): Promise<Turma | Error> {
+        if (await cursor.findOne({ where: { id_turma }})) {
+            return new Error("Unidade já cadastrada")
+        }
+        const Turma = cursor.create({
+            id_turma,
+            fk_curso,
+            data_inicio,
+            data_fim,
+            horas_aula_dia,
+        })
+        await cursor.save(Turma)
+
+        return Turma
+    }
+}
 
 export class CreateCursoService {}
 
--- src/services/serviceUnidade.ts
+++ src/services/serviceUnidade.ts
@@ -1,7 +1,38 @@
-import Curso from "../databases/models/curso"
+import Unidade from "../databases/models/unidade"
 import { AppDataSource } from "../databases/connections/data-source"
 
-const cursor = AppDataSource.getRepository(Curso)
+
+const cursor = AppDataSource.getRepository(Unidade)
+
+type newUnidadeRequest = {
+    descricao_unidade: string
+    carga_horaria_unidade: number
+    ordem: number
+    fk_curso: string
+}
+
+export class CreateunidadeService {
+    async execute({
+        descricao_unidade,
+        carga_horaria_unidade,
+        ordem,
+        fk_curso,
+    }: newUnidadeRequest): Promise<Unidade | Error> {
+        if (await cursor.findOne({ where: { descricao_unidade } })) {
+            return new Error("Unidade já cadastrada")
+        }
+        const Unidade =  cursor.create({
+            descricao_unidade,
+            carga_horaria_unidade,
+            ordem,
+            fk_curso,
+        })
+        await cursor.save(Unidade)
+
+        return Unidade
+    }
+}
+
 
 export class CreateCursoService {}
 
