dd200825d221935b3772788f1e48dcaa1d5f93bc
Pedro Lucas <pedroldlima52@gmail.com>
2023-06-01 11:53:55 -0300

    class service - Pedro Lucas

--- src/services/serviceUnidade.ts
+++ src/services/serviceUnidade.ts
@@ -1,45 +1,74 @@
-import Unidade from "../databases/models/unidade"
-import { AppDataSource } from "../databases/connections/data-source"
+
+import Unidade from "../databases/models/unidade";
+import { AppDataSource } from "../databases/connections/data-source";
 
 
-const cursor = AppDataSource.getRepository(Unidade)
+const cursor = AppDataSource.getRepository(Unidade);
 
 type newUnidadeRequest = {
-    descricao_unidade: string
-    carga_horaria_unidade: number
-    ordem: number
-    fk_curso: string
-}
+  descricao_unidade: string;
+  carga_horaria_unidade: number;
+  ordem: number;
+  fk_curso: string;
+};
 
-export class CreateunidadeService {
-    async execute({
-        descricao_unidade,
-        carga_horaria_unidade,
-        ordem,
-        fk_curso,
-    }: newUnidadeRequest): Promise<Unidade | Error> {
-        if (await cursor.findOne({ where: { descricao_unidade } })) {
-            return new Error("Unidade já cadastrada")
-        }
-        const Unidade =  cursor.create({
-            descricao_unidade,
-            carga_horaria_unidade,
-            ordem,
-            fk_curso,
-        })
-        await cursor.save(Unidade)
+type findOneUnidadeRequest = {
+  descricao_unidade: string;
+};
 
-        return Unidade
+export class CreateUnidadeService {
+  async execute({
+    descricao_unidade,
+    carga_horaria_unidade,
+    ordem,
+    fk_curso,
+  }: newUnidadeRequest): Promise<Unidade | Error> {
+    if (await cursor.findOne({ where: { descricao_unidade } })) {
+      return new Error("Unidade já cadastrada");
     }
+    const unidade = cursor.create({
+      descricao_unidade,
+      carga_horaria_unidade,
+      ordem,
+      fk_curso,
+    });
+    await cursor.save(unidade);
+    return unidade;
+  }
 }
 
+export class ReadAllUnidadeService {
+  async execute(): Promise<Unidade[]> {
+    const unidades = await cursor.find();
+    return unidades;
+  }
+}
 
-export class CreateCursoService {}
+export class ReadOneUnidadeService {
+  async execute({ descricao_unidade }: findOneUnidadeRequest): Promise<Unidade | Error> {
+    const unidade = await cursor.findOne({ where: { descricao_unidade } });
 
-export class ReadAllCursoService {}
+    if (!unidade) {
+      return new Error("Unidade não encontrada!");
+    }
 
-export class ReadOneCursoService {}
+    return unidade;
+  }
+}
 
-export class UpdateCursoService {}
+export class UpdateUnidadeService {
+  async execute() {
 
-export class DeleteCursoService {}
+  }
+}
+
+export class DeleteUnidadeService {
+  async execute({ descricao_unidade }: findOneUnidadeRequest ) {
+    const unidade = await cursor.findOne({ where: {descricao_unidade }})
+    if (!unidade) {
+    return new Error("Não encontrado a turma!")
+    }
+    await cursor.delete(unidade)
+    return unidade
+  }
+}
